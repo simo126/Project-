e4066192cbb67dc9ed418c02087ca613ac31f6d9
SBAITI MOHAMMED <45595271+simo126@users.noreply.github.com>
2023-12-28 22:07:19 +0100

    FinalVersion

--- SandwichMaker/script.js
+++ SandwichMaker/script.js
@@ -1,13 +1,21 @@
+//to add ingredients to the burger and and adding its value to the final price
 function AddElement(x, y) {
-  let v = document.getElementById("insideburger");
+  let v = document.getElementById("insidesandwich");
   let counterElement = document.querySelector(".counter-" + x);
   let currentValue = parseInt(counterElement.textContent);
   if (currentValue < 3) {
     v.innerHTML += `<div id="${x}"></div>`;
     counterElement.textContent = currentValue + 1;
     updatePrixFinal(y);
+  } else {
+    alert(
+      "We are sorry but you cant add more than 3 ingredients!" + "\u{1F622}"
+      //alert the customer that he cant get more than 3 elements from one ingredient
+      //purpose to not make the sandwich so much big its illogical having a 36 piece of meat in one sandwich hhhh
+    );
   }
 }
+//to update the Finale Price
 function updatePrixFinal(priceIncrement) {
   let finalePrice = document.querySelector(".final");
   let currentPriceText = finalePrice.textContent;
@@ -16,8 +24,9 @@ function updatePrixFinal(priceIncrement) {
 
   finalePrice.textContent = newPrice + " dh";
 }
+//to remove any ingredient you want from the burger and also reducing the price of it.
 function remElement(x, y) {
-  let v = document.getElementById("insideburger");
+  let v = document.getElementById("insidesandwich");
   let elementToRemove = document.getElementById(x);
   let counterElement = document.querySelector(".counter-" + x);
   let currentValue = parseInt(counterElement.textContent);
@@ -28,8 +37,12 @@ function remElement(x, y) {
       counterElement.textContent = currentValue - 1;
       updatePrixFinal(y);
     }
+  } else {
+    alert("You cant remove something that doesnt exist!" + "\u{1F604}");
+    //alerrt the client because you cant go below wero ingredients
   }
 }
+//to remove all elements inside of the burger and make the price of the sandwich equal 0
 function removeAll() {
   let counters = document.querySelectorAll(
     ".counter-ketchup, .counter-mayo, .counter-mustard, .counter-biggy, .counter-beef, .counter-turkey, .counter-fish, .counter-chicken, .counter-swiss, .counter-american, .counter-cheedar, .counter-mozarella,.counter-lettuce, .counter-tomato, .counter-onion, .counter-pickles"
@@ -38,8 +51,8 @@ function removeAll() {
     counter.textContent = "0";
   });
 
-  let container = document.getElementById("insideburger");
+  let container = document.getElementById("insidesandwich");
   container.innerHTML = "";
-  let finalePrice = document.querySelector(".Final-price");
-  finalePrice.textContent = "Final price: " + 0 + "dh";
+  let finalePrice0 = document.querySelector(".prix.final");
+  finalePrice0.textContent = "0 dh";
 }
